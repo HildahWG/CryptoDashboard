d2640f97342c361f0d5ef362a6261d17f2154e72
HildahWG <hildahgakuru852@gmail.com>
2025-06-09 01:32:46 +0300

    window status bar

--- app/src/main/java/com/example/cryptodashboard/view/MainActivity.kt
+++ app/src/main/java/com/example/cryptodashboard/view/MainActivity.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -105,7 +108,8 @@ class MainActivity : ComponentActivity() {
         }
 
         Column(modifier = modifier.fillMaxSize()
-            .background(customColors.backgroundColor).padding(16.dp)) {
+            .background(customColors.backgroundColor).padding(WindowInsets.systemBars.asPaddingValues()) // Handles status bar, nav bar
+            .padding(horizontal = 16.dp, vertical = 8.dp)) {
             OutlinedTextField(
 
                 value = searchQuery,
